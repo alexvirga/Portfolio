813dd950fb64a41e1d0e29628e23e68c3fefe7f5
alexvirga <alexander.virga@gmail.com>
2019-12-02 16:37:01 -0500

    Updates

---
+++ precache-manifest.2e1381ab3ce1ab8e1237883a6a62b22e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1534d0cbff8086d268e0445efbc6be3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b77d2a3c5aca971c7729",
+    "url": "/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "ac2c7cb3f43e0671ecf5",
+    "url": "/static/css/main.6295874b.chunk.css"
+  },
+  {
+    "revision": "b77d2a3c5aca971c7729",
+    "url": "/static/js/2.365de378.chunk.js"
+  },
+  {
+    "revision": "ac2c7cb3f43e0671ecf5",
+    "url": "/static/js/main.a233e257.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  },
+  {
+    "revision": "663fccd615f0abbd66d320aeeed04c3a",
+    "url": "/static/media/CircularSpUI-Bold.663fccd6.otf"
+  },
+  {
+    "revision": "c420fc42d62f8be47be6d848703c96c3",
+    "url": "/static/media/CircularSpUI-Book.c420fc42.otf"
+  },
+  {
+    "revision": "ea61b284b5b33e03ae6f7147d66b0731",
+    "url": "/static/media/CircularSpUIm40-Bold.ea61b284.otf"
+  },
+  {
+    "revision": "bdd2d58b032c0cd92e703153251ff53e",
+    "url": "/static/media/CircularSpUIm5-Book.bdd2d58b.otf"
+  }
+]);
