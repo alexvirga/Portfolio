ad41de26e97c7f4cbb28fcc72ff7563bd08fa600
alexvirga <alexander.virga@gmail.com>
2019-12-02 15:14:59 -0500

    Updates

---
+++ precache-manifest.6b85e22f1d03158da831022f041cee3d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8fe01446522f3406069eff857b364b5",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1ea3c70836539aa16ccc",
+    "url": "/Portfolio/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "dc54bdcf67121cf01205",
+    "url": "/Portfolio/static/css/main.c1bf55d8.chunk.css"
+  },
+  {
+    "revision": "1ea3c70836539aa16ccc",
+    "url": "/Portfolio/static/js/2.e33d2726.chunk.js"
+  },
+  {
+    "revision": "dc54bdcf67121cf01205",
+    "url": "/Portfolio/static/js/main.4d029a87.chunk.js"
+  },
+  {
+    "revision": "dd65a4e71a8b70668f2c",
+    "url": "/Portfolio/static/js/runtime-main.6a869909.js"
+  },
+  {
+    "revision": "663fccd615f0abbd66d320aeeed04c3a",
+    "url": "/Portfolio/static/media/CircularSpUI-Bold.663fccd6.otf"
+  },
+  {
+    "revision": "c420fc42d62f8be47be6d848703c96c3",
+    "url": "/Portfolio/static/media/CircularSpUI-Book.c420fc42.otf"
+  },
+  {
+    "revision": "ea61b284b5b33e03ae6f7147d66b0731",
+    "url": "/Portfolio/static/media/CircularSpUIm40-Bold.ea61b284.otf"
+  },
+  {
+    "revision": "bdd2d58b032c0cd92e703153251ff53e",
+    "url": "/Portfolio/static/media/CircularSpUIm5-Book.bdd2d58b.otf"
+  }
+]);
